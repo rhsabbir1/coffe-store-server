f99018e7ee1554dc3af818610a08905814be3baa
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-09 16:25:12 +0600

    deleted coffe

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const cors = require('cors');
@@ -28,19 +28,25 @@ async function run() {
 
     const coffeCollection = client.db("coffeBD").collection("coffe");
 
-    app.get('/coffe', async(req, res)=>{
+    app.get('/coffe', async (req, res) => {
       const curcor = coffeCollection.find()
       const result = await curcor.toArray()
       res.send(result)
     })
 
-    app.post('/coffe', async(req , res)=>{
+
+    app.post('/coffe', async (req, res) => {
       const newCoffe = req.body;
       const result = await coffeCollection.insertOne(newCoffe);
       res.send(result)
     })
 
-
+    app.delete('/coffe/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await coffeCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -53,10 +59,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req, res) => {
-    res.send("server is running")
+app.get('/', (req, res) => {
+  res.send("server is running")
 })
 
-app.listen(port , ()=>{
-    console.log(`server is runnin port on ${port}`)
+app.listen(port, () => {
+  console.log(`server is runnin port on ${port}`)
 })
